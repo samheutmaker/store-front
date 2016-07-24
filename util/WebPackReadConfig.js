997e5638bc8e213eaa5d08c83cecf5a7122aa244
SamHeutmaker <samheutmaker@gmail.com>
2016-07-23 20:30:33 -0700

    Remove es6 from new webpack plugin

--- util/WebPackReadConfig.js
+++ util/WebPackReadConfig.js
@@ -6,14 +6,14 @@ const finalFilePath = __dirname + '/..' + filePathFromRoot;
 
 module.exports = exports = function() {
 
-	let files = fs.readdirSync(finalFilePath, {}, 'utf8');
-	let productsArray = [];
+	const files = fs.readdirSync(finalFilePath, {}, 'utf8');
+	const productsArray = [];
 	files.forEach( file => {
 		if(file.substring(file.length - 5, file.length) == '.json')
 		productsArray.push(JSON.parse(fs.readFileSync(finalFilePath + file)));
 	});
 
-	let finalProductsString = JSON.stringify(productsArray);
+	const finalProductsString = JSON.stringify(productsArray);
 
 	var products = {
 		PRODUCTS: finalProductsString
